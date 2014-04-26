757dbf0b60c732a88de58cc7ff09981cb9b1ea32
Michael Fink <michael.fink@asamnet.de>
2014-04-26 11:13:14 +0200

    added setup of visual studio and pause command

--- src/CoverityScan.cmd
+++ src/CoverityScan.cmd
@@ -1,3 +1,5 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-7.0.2\bin
 cov-build --dir cov-int msbuild RemotePhotoTool.sln /property:Configuration=Release /target:Rebuild
 "C:\Program Files\7-Zip\7z.exe" a cov-int.zip .\cov-int
+pause
