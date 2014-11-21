845c02a45e675815aa210837e8498cef48eaee67
Michael Fink <michael.fink@asamnet.de>
2014-11-21 16:17:24 +0100

    removed thread window message processing in AsyncReleaseControlThread, since there are no thread window messages in this thread

--- src/CanonControl/AsyncReleaseControlThread.cpp
+++ src/CanonControl/AsyncReleaseControlThread.cpp
@@ -15,28 +15,12 @@ void AsyncReleaseControlThread::Run()
    Thread::SetName(_T("AsyncReleaseControlThread"));
 
    // run message queue
-   MSG msg = { 0 };
    while (!m_bFinished)
    {
-      // process window messages
-      if (::PeekMessage(&msg, nullptr, 0, 0, PM_NOREMOVE))
-      {
-         BOOL bRet = ::GetMessage(&msg, nullptr, 0, 0);
-         if (!bRet)
-            break;   // WM_QUIT, exit message loop
+      // process asio handlers, if any
+      boost::system::error_code ec;
 
-         ATLTRACE(_T("HWND=%08x MSG=%04x\n"), msg.hwnd, msg.message);
-
-         ::TranslateMessage(&msg);
-         ::DispatchMessage(&msg);
-      }
-      else
-      {
-         // process asio handlers, if any
-         boost::system::error_code ec;
-
-         m_ioService.poll_one(ec);
-      }
+      m_ioService.poll_one(ec);
 
       // skip current time slice
       Sleep(1);
