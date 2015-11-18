be28440e9701907531fbe9c0df95e329db5bb24b
Michael Fink <michael.fink@asamnet.de>
2015-11-18 20:28:48 +0100

    added some more tests for CameraScriptProcessor

--- src/LuaScripting/LuaScripting.UnitTest/TestCameraScriptProcessor.cpp
+++ src/LuaScripting/LuaScripting.UnitTest/TestCameraScriptProcessor.cpp
@@ -1,6 +1,6 @@
 //
 // RemotePhotoTool - remote camera control software
-// Copyright (C) 2008-2014 Michael Fink
+// Copyright (C) 2008-2015 Michael Fink
 //
 /// \file TestCameraScriptProcessor.cpp Tests for CameraScriptProcessor class
 //
@@ -9,6 +9,8 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "CameraScriptProcessor.hpp"
+#include "LuaScheduler.hpp"
+#include "Event.hpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -22,14 +24,57 @@ namespace LuaScriptingUnitTest
       TEST_METHOD(TestDefaultCtor)
       {
          CameraScriptProcessor csp;
+
+         Assert::IsTrue(csp.GetScheduler().CurrentExecutionState() == LuaScheduler::stateIdle,
+            _T("current execution state must be idle"));
       }
 
-      /// tests default ctor
-      TEST_METHOD(TestStartStop)
+      /// tests running without script loaded
+      TEST_METHOD(TestEmpty)
       {
          CameraScriptProcessor csp;
-         csp.LoadScript(_T("C:\\Users\\Michael\\SkyDrive\\Dokumente\\RemotePhotoTool.lua"));
+
+         // run
          csp.Run();
+
+         // check
+         Assert::IsTrue(csp.GetScheduler().CurrentExecutionState() == LuaScheduler::stateIdle,
+            _T("current execution state must be idle"));
+      }
+
+      /// tests Run() and Stop() methods
+      TEST_METHOD(TestRunStop)
+      {
+         CameraScriptProcessor csp;
+         csp.LoadSourceString(_T("App = { run = function() print(\"no-op\") end; }"));
+         csp.Run();
+         csp.Stop();
+      }
+
+      /// tests running script multiple times
+      TEST_METHOD(TestRunningMultipleTimes)
+      {
+         CameraScriptProcessor csp;
+
+         Event evtWaitIdle(true, false); // manual-reset
+         csp.GetScheduler().SetExecutionStateChangedHandler([&](LuaScheduler::T_enExecutionState enExecutionState)
+         {
+            if (enExecutionState == LuaScheduler::stateIdle)
+               evtWaitIdle.Set();
+         });
+
+         for (int i = 0; i < 100; i++)
+         {
+            csp.LoadSourceString(_T("App = { run = function() print(\"no-op\") end; }"));
+
+            evtWaitIdle.Reset();
+
+            csp.Run();
+
+            Assert::IsTrue(evtWaitIdle.Wait(), _T("waiting must not fail"));
+
+            csp.Stop();
+         }
       }
    };
 }
