a4e4a6a0b11233a8da23a2fec76c7c3675368f71
Michael Fink <michael.fink@asamnet.de>
2016-01-27 21:47:56 +0100

    added project Logic and unit tests Logic.UnitTest

---
+++ src/Logic/stdafx.h
@@ -0,0 +1,12 @@
+//
+// RemotePhotoTool - remote camera control software
+// Copyright (C) 2008-2016 Michael Fink
+//
+/// \file Logic\stdafx.h Precompiled header support
+//
+#pragma once
+
+// includes
+#include <SDKDDKVer.h>
+
+#include "Atl.hpp"
