cceecd0fca00f3e3fea5e2249c2f6b2e53d052fc
Michael Fink <michael.fink@asamnet.de>
2016-02-05 16:59:39 +0100

    using T_enImageType from Logic project

--- src/RemotePhotoTool/ImageFileManager.hpp
+++ src/RemotePhotoTool/ImageFileManager.hpp
@@ -1,6 +1,6 @@
 //
 // RemotePhotoTool - remote camera control software
-// Copyright (C) 2008-2014 Michael Fink
+// Copyright (C) 2008-2016 Michael Fink
 //
 /// \file ImageFileManager.hpp Image file manager
 //
@@ -8,17 +8,7 @@
 
 // includes
 #include "AppSettings.hpp"
-
-/// image file manager image type
-enum T_enImageType
-{
-   imageTypeNormal = 0, ///< normal image
-   imageTypeHDR = 1,    ///< image of a HDR bracketing series
-   imageTypePano = 2,   ///< image of a panorama take
-   imageTypeHDRPano = 3,   ///< image of a HDR panorama bracketing series
-   imageTypeTimelapse = 4,    ///< image of a timelapse series
-   imageTypePhotostack = 5,   ///< image of a photostack series
-};
+#include "ImageType.hpp"
 
 /// image file manager
 class ImageFileManager
