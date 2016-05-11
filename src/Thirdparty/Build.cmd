326481f8fa0ae17f22137ff4c054525939302f5c
Michael Fink <michael.fink@asamnet.de>
2016-05-11 22:40:00 +0200

    updated Setup to use new gPhoto2 libraries

--- src/Thirdparty/Build.cmd
+++ src/Thirdparty/Build.cmd
@@ -1,7 +1,15 @@
+REM
+REM RemotePhotoTool - remote camera control software
+REM Copyright (C) 2008-2016 Michael Fink
+REM
+REM Build script for Thirdparty libraries
+REM
+
 set OUTDIR=%1
+
 mkdir %OUTDIR% 2> nul
 copy CDSDK\Redist\*.* %OUTDIR%
 copy EDSDK\dll\*.* %OUTDIR%
 copy PSREC\PSReCSDK\redist\*.* %OUTDIR%
 copy Scintilla\bin\*.* %OUTDIR%
-copy gphoto2\bin\*.* %OUTDIR%
+xcopy gphoto2\bin\*.* %OUTDIR% /S /Y
--- src/Thirdparty/Clean.cmd
+++ src/Thirdparty/Clean.cmd
@@ -1,4 +1,13 @@
+REM
+REM RemotePhotoTool - remote camera control software
+REM Copyright (C) 2008-2016 Michael Fink
+REM
+REM Clean script for Thirdparty libraries
+REM
+
 set OUTDIR=%1
+
+REM CDSDK
 del %OUTDIR%cdDcd.dll 2> nul
 del %OUTDIR%cdDvlp.dll 2> nul
 del %OUTDIR%cdParse.dll 2> nul
@@ -8,29 +17,37 @@ del %OUTDIR%CDPTPCLS.dll 2> nul
 del %OUTDIR%CDPTPCLT.dll 2> nul
 del %OUTDIR%CDRAPCLS.dll 2> nul
 del %OUTDIR%CDSDK.dll 2> nul
+del %OUTDIR%EWatch.exe 2> nul
+del %OUTDIR%GetPicture.exe 2> nul
+del %OUTDIR%IWrap.dll 2> nul
+del %OUTDIR%psCamDat.dll 2> nul
+
+REM EDSDK
 del %OUTDIR%DPPDLL.dll 2> nul
 del %OUTDIR%DPPLibCom.dll 2> nul
 del %OUTDIR%DPPRSC.dll 2> nul
 del %OUTDIR%EDSDK.dll 2> nul
+del %OUTDIR%EDSDK_25.dll 2> nul
 del %OUTDIR%EdsImage.dll 2> nul
-del %OUTDIR%EWatch.exe 2> nul
-del %OUTDIR%GetPicture.exe 2> nul
-del %OUTDIR%IWrap.dll 2> nul
 del %OUTDIR%MLib.dll 2> nul
+del %OUTDIR%Ucs32P.dll 2> nul
+
+REM PSREC
 del %OUTDIR%PRLIB.dll 2> nul
 del %OUTDIR%PRSDK.dll 2> nul
-del %OUTDIR%psCamDat.dll 2> nul
-del %OUTDIR%Ucs32P.dll 2> nul
-del %OUTDIR%lua.properties 2> nul
+
+REM Scintilla
 del %OUTDIR%SciLexer.dll 2> nul
+del %OUTDIR%lua.properties 2> nul
 
 REM gPhoto2 files
-del %OUTDIR%SciLexer.dll 2> nul
-del %OUTDIR%libusb1.dll 2> nul
-del %OUTDIR%ptp2.dll 2> nul
-del %OUTDIR%ptpip.dll 2> nul
-del %OUTDIR%canon.dll 2> nul
-del %OUTDIR%disk.dll 2> nul
 del %OUTDIR%libgphoto2.dll 2> nul
 del %OUTDIR%libgphoto2_port.dll 2> nul
-del %OUTDIR%libusb-1.0.dll 2> nul
+del %OUTDIR%camlibs\canon.dll 2> nul
+del %OUTDIR%camlibs\ptp2.dll 2> nul
+del %OUTDIR%iolibs\disk.dll 2> nul
+del %OUTDIR%iolibs\libusb-1.0.dll 2> nul
+del %OUTDIR%iolibs\libusb1.dll 2> nul
+del %OUTDIR%iolibs\ptpip.dll 2> nul
+rmdir %OUTDIR%camlibs 2> nul
+rmdir %OUTDIR%iolibs 2> nul
