0ff87ed4b6e2c00c4f509da138aafb2055c9a69a
Michael Fink <michael.fink@asamnet.de>
2016-07-20 19:06:32 +0200

    updated CoverityScan to version 8.5.0

--- src/CoverityScan.cmd
+++ src/CoverityScan.cmd
@@ -9,7 +9,7 @@ rmdir .\cov-int /s /q 2> nul
 del cov-int.zip 2> nul
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
-set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-7.7.0.4\bin
+set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-8.5.0\bin
 
 cov-build --dir cov-int msbuild RemotePhotoTool.sln /m /property:Configuration=Release /property:Platform=Win32 /target:Rebuild
 "%ProgramFiles%\7-Zip\7z.exe" a cov-int.zip .\cov-int
