eb5b11cc5a37e9e841f1ea4b51e233b2781a41c0
Michael Fink <michael.fink@asamnet.de>
2017-05-23 18:47:01 +0200

    updated Coverity Scan to version 8.7.0

--- src/CoverityScan.cmd
+++ src/CoverityScan.cmd
@@ -1,6 +1,6 @@
 REM
 REM RemotePhotoTool - remote camera control software
-REM Copyright (C) 2008-2016 Michael Fink
+REM Copyright (C) 2008-2017 Michael Fink
 REM
 REM runs Coverity analysis build
 REM
@@ -9,9 +9,9 @@ rmdir .\cov-int /s /q 2> nul
 del cov-int.zip 2> nul
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
-set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-8.5.0\bin
+set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-8.7.0\bin
 
-cov-build --dir cov-int msbuild RemotePhotoTool.sln /m /property:Configuration=Release /property:Platform=Win32 /target:Rebuild
+cov-build --dir cov-int msbuild RemotePhotoTool.sln /property:Configuration=Release /property:Platform=Win32 /target:Rebuild
 "%ProgramFiles%\7-Zip\7z.exe" a cov-int.zip .\cov-int
 
 pause
