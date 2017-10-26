42676ea7ff720fbd07b01e64499b8fadaf8478aa
Michael Fink <michael.fink@asamnet.de>
2017-10-26 21:28:33 +0200

    updated SonarQube build script

--- src/SonarQube.cmd
+++ src/SonarQube.cmd
@@ -14,7 +14,7 @@ REM
 cd Thirdparty\SonarQube
 
 "%ProgramFiles%\7-Zip\7z.exe" x -y build-wrapper-win-x86.zip
-"%ProgramFiles%\7-Zip\7z.exe" x -y -osonar-scanner-msbuild sonar-scanner-msbuild-2.3.2.573.zip
+"%ProgramFiles%\7-Zip\7z.exe" x -y -osonar-scanner-msbuild sonar-scanner-msbuild-3.0.2.656.zip
 cd ..\..
 
 PATH=%PATH%;%CD%\Thirdparty\SonarQube\build-wrapper-win-x86;%CD%\Thirdparty\SonarQube\sonar-scanner-msbuild
@@ -24,6 +24,8 @@ REM Build using SonarQube scanner for MSBuild
 REM
 rmdir .\bw-output /s /q 2> nul
 
+msbuild RemotePhotoTool.sln /m /property:Configuration=Release,Platform=Win32 /target:Clean
+
 SonarQube.Scanner.MSBuild.exe begin ^
     /k:"RemotePhotoTool" ^
     /v:"1.6.0" ^
@@ -32,7 +34,7 @@ SonarQube.Scanner.MSBuild.exe begin ^
     /d:"sonar.organization=vividos-github" ^
     /d:"sonar.login=6a2bdc8a9052902bc257547dfc2a6d79e5089aae"
 
-build-wrapper-win-x86-64.exe --out-dir bw-output msbuild RemotePhotoTool.sln /m /property:Configuration=Release /property:Platform=Win32 /target:Rebuild
+build-wrapper-win-x86-64.exe --out-dir bw-output msbuild RemotePhotoTool.sln /m /property:Configuration=Release,Platform=Win32 /target:Build
 
 SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=6a2bdc8a9052902bc257547dfc2a6d79e5089aae"
 
