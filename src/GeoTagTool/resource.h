d02907ad91bd4e7f4ad5689fbca2043d88b4d83b
Michael Fink <michael.fink@asamnet.de>
2018-03-01 07:41:28 +0100

    Visual Studio resource editor reformatted my resource files

--- src/GeoTagTool/resource.h
+++ src/GeoTagTool/resource.h
@@ -1,22 +1,20 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by GeoTagTool.RC
+// Used by GeoTagTool.rc
 //
-
-
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-#define IDD_GEOTAGTOOL_FORM	        129
-#define IDD_SERIAL_PORT             130
-#define IDC_COMBO_SERIALPORT        1000
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_GEOTAGTOOL_FORM             129
+#define IDD_SERIAL_PORT                 130
+#define IDC_COMBO_SERIALPORT            1000
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	131
-#define _APS_NEXT_CONTROL_VALUE		1001
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32775
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
