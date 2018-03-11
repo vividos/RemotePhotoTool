5c07aebb5b015bcee59454fb21f1de13ea597882
Michael Fink <michael.fink@asamnet.de>
2018-03-11 11:15:58 +0100

    updated Coverity Scan cmd script for current cov-analysis tool, and added NuGet restore

--- src/CoverityScan.cmd
+++ src/CoverityScan.cmd
@@ -1,6 +1,6 @@
 REM
 REM RemotePhotoTool - remote camera control software
-REM Copyright (C) 2008-2017 Michael Fink
+REM Copyright (C) 2008-2018 Michael Fink
 REM
 REM runs Coverity analysis build
 REM
@@ -8,10 +8,18 @@ REM
 rmdir .\cov-int /s /q 2> nul
 del cov-int.zip 2> nul
 
+REM set up build environment
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-8.7.0\bin
 
-cov-build --dir cov-int msbuild RemotePhotoTool.sln /property:Configuration=Release /property:Platform=Win32 /target:Rebuild
+set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-2017.07\bin
+
+REM restore NuGet packages
+..\tools\nuget restore RemotePhotoTool.sln
+
+REM build solution
+cov-build --dir cov-int  ^
+   msbuild RemotePhotoTool.sln /property:Configuration=Release /property:Platform=Win32 /target:Rebuild
+
 "%ProgramFiles%\7-Zip\7z.exe" a cov-int.zip .\cov-int
 
 pause
