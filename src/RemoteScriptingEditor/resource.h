6aedeba8ae865a31fc6d41c849b1139b3244026e
Michael Fink <michael.fink@asamnet.de>
2019-01-14 07:12:34 +0100

    converted RemotePhotoTool.rc and RemoteScriptingEditor.rc from ResEdit format flavor to Visual Studio 2017 resource editor flavor

--- src/RemoteScriptingEditor/resource.h
+++ src/RemoteScriptingEditor/resource.h
@@ -1,9 +1,20 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by RemoteScriptingEditor.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_PANE_SCRIPT_STATUS          129
+#define IDC_STATIC_ABOUT_VERSION        1024
+#define IDC_STATIC_ABOUT_INFO           1025
 
-#define IDD_ABOUTBOX                            100
-#define IDR_MAINFRAME                           128
-#define IDR_PANE_SCRIPT_STATUS                  129
-#define IDC_STATIC_ABOUT_VERSION                1024
-#define IDC_STATIC_ABOUT_INFO                   1025
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
